6269aa84a0c03566f8cb77a42ba2bc0ea013760f
Marcos Barbosa Group <joel@marcosbarbosagroup.com>
2026-09-15 18:22:34 -0300

    chore: limpiar scripts temp

--- scripts/_cleanup_meeting.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path.insert(0, "/home/frappe/frappe-bench/apps")
-import frappe
-frappe.init("crm.marcosbarbosagroup.com", sites_path="/home/frappe/frappe-bench/sites")
-frappe.connect(); frappe.flags.in_install_db = False
-for n in frappe.get_all("CRM Lead", filters={"email": "zzreunion@example.com"}, pluck="name"):
-    frappe.delete_doc("CRM Lead", n, force=True, ignore_permissions=True)
-frappe.db.commit()
-print("leads:", frappe.db.count("CRM Lead"))
